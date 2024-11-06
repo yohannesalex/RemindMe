4b99d51a7a90727a0eed6c084b431667ea4cfee0
yohannesalex <yohannesalemayehu624@gmail.com>
2024-11-06 21:44:06 +0300

    updated the url path

--- remind/obj/Debug/net8.0/remind.AssemblyInfo.cs
+++ remind/obj/Debug/net8.0/remind.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("remind")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1faec75aa078bb679a95dd0b195688cc4737e57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80dc6dc1196386ab4913c446c5e7cbe510fec67")]
 [assembly: System.Reflection.AssemblyProductAttribute("remind")]
 [assembly: System.Reflection.AssemblyTitleAttribute("remind")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
